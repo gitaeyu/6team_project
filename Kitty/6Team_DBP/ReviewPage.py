a2aa38f9403f639c10abfc74f92c20bbd7d05aa5
ElisaBluebell <selfishracer@hotmail.com>
2022-12-29 19:33:41 +0900

    ElisaBluebell

--- Kitty/6Team_DBP/ReviewPage.py
+++ Kitty/6Team_DBP/ReviewPage.py
@@ -6,6 +6,7 @@ from PyQt5.QtGui import *
 
 # 리뷰 페이지 클래스
 class ReviewPage(QWidget):
+
     def __init__(self):
         super().__init__()
         # 외부에서 받아올 유저, 레스토랑 정보[유저 id, 레스토랑 상가업소번호]
@@ -24,13 +25,14 @@ class ReviewPage(QWidget):
 
         # 프로그램 세팅
         self.setWindowTitle('리뷰')
-
         self.setGeometry(0, 0, 800, 600)
         self.show()
 
+    # 공통 ui 설정
     def set_ui(self):
         self.set_button()
 
+    # 버튼 세팅
     def set_button(self):
         self.return_button = QPushButton('돌아가기', self)
         self.return_button.setGeometry(700, 560, 80, 20)
@@ -59,10 +61,12 @@ class ReviewPage(QWidget):
         self.review_announce_label = QLabel('100자 이내로 작성해주세요.', self)
         self.review_announce_label.setGeometry(25, 25, 200, 20)
 
-        # 식당 점수 평균 구함
+        # 식당 점수를 review_score 함수에 입력
         self.review_score = self.score_calc(ui_length)
+        
         # 별점 라벨(김연수 아이디어 추가)
         self.review_score_label = QLabel('★★★★★', self)
+        # 별을 평점에 맞게 잘라서 출력함
         self.review_score_label.setGeometry(25, 130, int(self.review_score * 12), 20)
 
         # self.i값에 받아 반복에 사용
@@ -74,25 +78,32 @@ class ReviewPage(QWidget):
 
         # 지점명이 있을 경우 지점명 더함
         if not self.save_review:
+            # 평가가 없을 경우 평가 없음 문구 출력
             self.nocoment_label = QLabel('평가가 없습니다.', self)
             self.nocoment_label.setGeometry(350, 300, 100, 20)
         else:
+            # 평가가 있을 경우 유저 리뷰 출력, 지점명이 있을 경우 지점명을 더함
             if self.save_review[0][3]:
+                # 출력문('식당명 지점명 별점 유저리뷰')
                 self.restaurant_name_label = QLabel(f'{self.save_review[0][2]}{self.save_review[0][3]}'
                                                     f'({self.review_score:.1f} / 5) 유저리뷰', self)
             else:
                 self.restaurant_name_label = QLabel(f'{self.save_review[0][2]}({self.review_score:.1f}'
                                                     f' / 5) 유저리뷰', self)
+            # 라벨 지오메트리
             self.restaurant_name_label.setGeometry(100, 130, 300, 20)
 
     # 평점 계산기
     def score_calc(self, length):
+        # 0개일 경우 0 반환
         if length == 0:
             return 0
         else:
             review_score = 0
+            # 평점을 모두 더해서
             for i in range(length):
                 review_score += self.save_review[i][4]
+            # 평가수로 나누어 평균을 구함
             return review_score / length
 
     # 리뷰 뷰어?
@@ -134,22 +145,30 @@ class ReviewPage(QWidget):
 
     # 리뷰 업로드
     def upload_review(self):
+        # 별점이 선택되지 않으면 리뷰가 등록되지 않음
         if self.score_combobox.currentText() == '별점 선택':
             QMessageBox.warning(self, '별점 선택', '별점이 선택되지 않았습니다.')
         else:
+            # 리뷰 등록 여부 확인
             upload = QMessageBox.question(self, '게시물 등록', '리뷰를 등록하시겠습니까?', QMessageBox.Yes | QMessageBox.No,
                                  QMessageBox.No)
             if upload == QMessageBox.Yes:
+                # 오토커밋 db오픈
                 conn = sqlite3.connect("shop2.db", isolation_level=None)
                 c = conn.cursor()
+                # 상호명과 지점명을 상가업소번호를 통해 받아옴
                 c.execute(f'select 상호명 from Gwangju where 상가업소번호={self.user_restaurant_info[1]}')
                 restaurant_name = c.fetchone()[0]
                 c.execute(f'select 지점명 from Gwangju where 상가업소번호={self.user_restaurant_info[1]}')
                 restaurant_branch = c.fetchone()[0]
+                # 지점명 유무에 따른 등록 db 차이
                 if not restaurant_branch:
                     c.execute(f'insert into 리뷰 (상가업소번호, ID, 상호명, 평점, 리뷰) values ("{self.user_restaurant_info[1]}", "{self.user_restaurant_info[0]}", "{restaurant_name}", {self.score_combobox.currentData()}, "{self.review_write_text.toPlainText()}")')
                 else:
                     c.execute(f'insert into 리뷰 ("{self.user_restaurant_info[1]}", "{self.user_restaurant_info[0]}", "{restaurant_name}", "{restaurant_branch}", {self.score_combobox.currentData()}, "{self.review_write_text.toPlainText()}")')
+                # 리뷰 등록 완료
+                QMessageBox.information(self, '리뷰 등록', '리뷰가 등록되었습니다.')
+                self.close()
             else:
                 pass
 
